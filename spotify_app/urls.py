b0a64d75e23e59c271c74e5fb777058b0764a26f
rohitgogi <rohit.gogi12@gmail.com>
2024-10-29 17:55:59 -0400

    integrated spotify api and got the main details

--- spotify_app/urls.py
+++ spotify_app/urls.py
@@ -1,7 +1,10 @@
-# spotify_app/urls.py
 from django.urls import path
 from . import views
 
+app_name = "spotify_app"  # Ensure this matches the namespace used in your project
 urlpatterns = [
     path('', views.home, name='home'),  # Home page route
+    path("login/", views.spotify_login, name="spotify_login"),
+    path("callback/", views.spotify_callback, name="spotify_callback"),
+    path("summary/", views.spotify_summary, name="spotify_summary"),
 ]
