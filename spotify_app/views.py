bf95f3c9f4783aab565f33902fc24086626daff9
aduyha04 <aduy04@gmail.com>
2024-11-19 15:22:29 -0500

    frontend changes

--- spotify_app/views.py
+++ spotify_app/views.py
@@ -120,23 +120,153 @@ def get_spotify_data(url, user_profile):
 
 
 # Function to get top artists, top songs, genres, listening time, and peak day
-def get_top_artists(user_profile):
+# Function to get top artists, top songs, genres, listening time, and peak day
+def get_top_artists(access_token):
     url = "https://api.spotify.com/v1/me/top/artists?limit=5&time_range=long_term"
-    data = get_spotify_data(url, user_profile)
-    print("Top Artists Data:", data)  # Debugging line
-    return [artist['name'] for artist in data.get('items', [])]
+    data = get_spotify_data(url, access_token)
+    artists = []
+    for artist in data.get('items', []):
+        artist_info = {
+            'name': artist['name'],
+            'image_url': artist['images'][0]['url'] if artist['images'] else None,  # Get the first image
+        }
+        artists.append(artist_info)
+    return artists
 
-def get_top_tracks(user_profile):
-    url = "https://api.spotify.com/v1/me/top/tracks?limit=5&time_range=long_term"
-    data = get_spotify_data(url, user_profile)
+def get_top_tracks(access_token):
+    url = "https://api.spotify.com/v1/me/top/tracks?limit=5&time_range=long_term"  # Last 12 months
+    data = get_spotify_data(url, access_token)
     print("Top Tracks Data:", data)  # Debugging line
-    return [track['name'] for track in data.get('items', [])]
+    return [
+        {
+            'name': track['name'],
+            'artist': track['artists'][0]['name'],  # Get the first artist's name
+            'image_url': track['album']['images'][0]['url']  # Get the album image URL
+        }
+        for track in data.get('items', [])
+    ]
 
-def get_top_genres(user_profile):
+def get_top_genres(access_token):
     url = "https://api.spotify.com/v1/me/top/artists?limit=50&time_range=long_term"
-    data = get_spotify_data(url, user_profile)
-    genres = [genre for artist in data.get('items', []) for genre in artist['genres']]
-    return sorted(set(genres), key=genres.count, reverse=True)[:5]
+    data = get_spotify_data(url, access_token)
+
+    genre_mapping = {
+        'k-pop girl group': 'k-pop',
+        'k-pop boy group': 'k-pop',
+        '5th gen k-pop': 'k-pop',
+        'pop rock': 'pop',
+        'indie pop': 'pop',
+        'pop punk': 'pop',
+        'dance-pop': 'pop',
+        'electropop': 'pop',
+        'classic rock': 'rock',
+        'hard rock': 'rock',
+        'indie rock': 'rock',
+        'punk rock': 'rock',
+        'alternative rock': 'rock',
+        'garage rock': 'rock',
+        'psychedelic rock': 'rock',
+        'trap': 'hip-hop',
+        'gangsta rap': 'hip-hop',
+        'underground rap': 'hip-hop',
+        'conscious hip-hop': 'hip-hop',
+        'rap rock': 'hip-hop',
+        'lo-fi rap': 'hip-hop',
+        'old school hip-hop': 'hip-hop',
+        'house': 'electronic',
+        'techno': 'electronic',
+        'dubstep': 'electronic',
+        'drum and bass': 'electronic',
+        'trance': 'electronic',
+        'electro house': 'electronic',
+        'synthwave': 'electronic',
+        'future bass': 'electronic',
+        'smooth jazz': 'jazz',
+        'vocal jazz': 'jazz',
+        'bebop': 'jazz',
+        'jazz fusion': 'jazz',
+        'soul jazz': 'jazz',
+        'electric blues': 'blues',
+        'delta blues': 'blues',
+        'baroque': 'classical',
+        'romantic era': 'classical',
+        'modern classical': 'classical',
+        'opera': 'classical',
+        'chamber music': 'classical',
+        'symphony': 'classical',
+        'neo soul': 'r&b',
+        'contemporary r&b': 'r&b',
+        'funk': 'r&b',
+        'motown': 'r&b',
+        'classic soul': 'r&b',
+        'alternative country': 'country',
+        'classic country': 'country',
+        'country rock': 'country',
+        'outlaw country': 'country',
+        'country pop': 'country',
+        'indie folk': 'folk',
+        'americana': 'folk',
+        'traditional folk': 'folk',
+        'folk rock': 'folk',
+        'acoustic': 'folk',
+        'dancehall': 'reggae',
+        'dub reggae': 'reggae',
+        'roots reggae': 'reggae',
+        'ska': 'reggae',
+        'reggaeton': 'latin',
+        'latin pop': 'latin',
+        'bachata': 'latin',
+        'salsa': 'latin',
+        'mariachi': 'latin',
+        'cumbia': 'latin',
+        'bossa nova': 'latin'
+    }
+
+    genres = []
+    for artist in data.get('items', []):
+        for genre in artist['genres']:
+            generalized_genre = genre_mapping.get(genre, genre)
+            genres.append(generalized_genre)
+
+    genre_counts = {}
+    for genre in genres:
+        genre_counts[genre] = genre_counts.get(genre, 0) + 1
+
+    sorted_genres = sorted(genre_counts, key=genre_counts.get, reverse=True)
+    top_genres = sorted_genres[:5]
+
+    while len(top_genres) < 5:
+        for genre in genre_mapping.values():
+            if genre not in top_genres:
+                top_genres.append(genre)
+                if len(top_genres) == 5:
+                    break
+
+    categories_url = "https://api.spotify.com/v1/browse/categories?limit=50"
+    headers = {"Authorization": f"Bearer {access_token}"}
+    categories_data = requests.get(categories_url, headers=headers).json()
+
+    mood_image_url = None
+    for category in categories_data.get('categories', {}).get('items', []):
+        if category['name'].lower() == 'mood':  # Check for the "mood" category
+            mood_image_url = category['icons'][0]['url']
+            break
+
+    genre_images = {}
+    for category in categories_data.get('categories', {}).get('items', []):
+        category_name = category['name'].lower()
+        category_image_url = category['icons'][0]['url']  # First image for the category
+        for genre in top_genres:
+            if genre.lower() in category_name:
+                genre_images[genre] = category_image_url
+                break  # Stop searching once a match is found
+
+    for genre in top_genres:
+        if genre not in genre_images:
+            genre_images[genre] = mood_image_url
+
+    genres_with_images = [{'name': genre, 'image_url': genre_images.get(genre, mood_image_url)} for genre in top_genres]
+    return genres_with_images
 
 def get_total_listening_time(user_profile):
     url = "https://api.spotify.com/v1/me/player/recently-played?limit=50"
